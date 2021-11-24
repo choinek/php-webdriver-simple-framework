9d1d7112682ef77185dc727b2979be79175f1e2d
Adrian Chojnicki <adrian.chojnicki@global4net.com>
2021-11-24 12:55:17 +0100

    + data file helper - import data file function

---
+++ src/Functions/DataFile.php
@@ -0,0 +1,32 @@
+<?php
+
+class DataFile {
+
+    /**
+     * @throws Exception
+     */
+    public static function importDataFile(string $name): array
+    {
+        if (!defined('APP_DIR')) {
+            throw new Exception ('APP_DIR was not defined in run.php');
+        }
+
+        $filePath = APP_DIR . DIRECTORY_SEPARATOR . 'data-file' . DIRECTORY_SEPARATOR . $name;
+
+        if (is_file($filePath)) {
+
+            $fileData = file_get_contents($filePath);
+            /** @var array $jsonData */
+            $jsonData = json_decode($fileData, true);
+
+            if ($jsonData) {
+                return $jsonData;
+            } else {
+                throw new Exception('Data file: ' . $name . ' - wrong or empty json.');
+            }
+
+        } else {
+            throw new \Exception ("Data file: $name was not found.");
+        }
+    }
+}
